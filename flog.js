e7b98faa5d28b8a132e029f2f301476d5776d229
Gilles Ruppert <gilles@madeofbytes.com>
2012-12-21 10:57:36 +0100

    1st draft of workable copy

--- flog.js
+++ flog.js
@@ -2,7 +2,85 @@
   def('flog', [], function() {
 
     // actual code...
+    var EMPTY = function () {};
+    var levels = {
+      'all': true
+    , 'info': true
+    , 'warn': true
+    , 'error': true
+    , 'silent': true
+    , 'quiet': true
+    };
+
+    function bindConsole(methodName) {
+      if (console && console[methodName] && console[methodName].bind) {
+        return console[methodName].bind(console);
+      } else {
+        return EMPTY;
+      }
+    }
+
+    // deal with IE8 & IE9's weird console.log (typeof == 'object')...
+    // http://whattheheadsaid.com/2011/04/internet-explorer-9s-problematic-console-object
+    if (Function.prototype.bind && console && typeof console.log === "object") {
+      var logMethods = ["log", "info", "warn", "error", "assert", "dir", "clear", "profile", "profileEnd", "trace"];
+
+      try {
+        // IE9
+        logMethods.forEach(function (method) {
+          console[method] = this.call(console[method], console);
+        }, Function.prototype.bind);
+      } catch(e) {
+        // IE8
+        function fixIe8(method) {
+          console[method] = Function.prototype.call.bind(console[method], console);
+        }
+        for (var i = 0; i < logMethods.length; i++) {
+          fixIe8(logMethods[i]);
+        }
+      }
+    }
+
+    // returns flog. Can be used as flog or as an instance
     return {
+      level: 'quiet',
+      log: EMPTY,
+      info: EMPTY,
+      warn: EMPTY,
+      error: EMPTY,
+
+      create: function () {
+        function F() {}
+        F.prototype = this;
+        return new F();
+      },
+
+      setLevel: function (level) {
+        if (level in levels) {
+          if (level === 'all') this.level = 'info';
+          else if (level === 'silent') this.level = 'quiet';
+          else this.level = level;
+        }
+        else this.level = 'quiet';
+
+        switch (this.level) {
+          case 'info':
+          case 'all':
+            this.log = bindConsole('log');
+            this.info = bindConsole('info');
+            /* falls through */
+          case 'warn':
+            this.warn = bindConsole('warn');
+            /* falls through */
+          case 'error':
+            this.error = bindConsole('error');
+            break;
+          default:
+            this.log = this.info = this.warn = this.error = EMPTY;
+        }
+
+        return this;
+      }
     };
 
   });
--- test/flog.test.js
+++ test/flog.test.js
@@ -1,6 +1,68 @@
 describe('flog', function () {
+  beforeEach(function () {
+    this.flog = flog.create();
+  });
+  afterEach(function () {
+    delete this.flog;
+  });
+
   it('should be an object', function () {
     expect(flog).to.be.an('object');
   });
 
+  describe('#create()', function () {
+    it('should return an instance of flog', function () {
+      expect(flog.create()).to.be.an('object');
+    });
+
+    it('should have flog as its prototype', function () {
+      expect(flog.isPrototypeOf(flog.create())).to.be(true);
+    });
+
+    it('should have the same level to start', function () {
+      expect(this.flog.log).to.be(flog.log);
+      expect(this.flog.info).to.be(flog.info);
+      expect(this.flog.warn).to.be(flog.warn);
+      expect(this.flog.error).to.be(flog.error);
+    });
+  });
+
+  describe('#setLevel()', function () {
+    it('should set the level', function () {
+      this.flog.setLevel('warn');
+      expect(this.flog.level).to.be('warn');
+    });
+
+    it('should set valid values', function () {
+      var levels = ['info', 'warn', 'error', 'quiet'],
+          i = 0;
+      for (; i < levels.length; i++) {
+        this.flog.setLevel(levels[i]);
+        expect(this.flog.level).to.be(levels[i]);
+      }
+    });
+
+    it('should not set valid values', function () {
+      var levels = ['foo', 'bar', 'baz'],
+          i = 0;
+      for (; i < levels.length; i++) {
+        this.flog.setLevel(levels[i]);
+        expect(this.flog.level).to.not.be(levels[i]);
+      }
+    });
+
+    it('should allow "info" and "quiet" aliases "all" and "silent"', function () {
+      expect(this.flog.setLevel('all').level).to.be('info');
+      expect(this.flog.setLevel('silent').level).to.be('quiet');
+    });
+
+    it('should set the level to "quiet" if the level value is not recognised', function () {
+      var levels = ['foo', 'bar', 'baz'],
+          i = 0;
+      for (; i < levels.length; i++) {
+        this.flog.setLevel(levels[i]);
+        expect(this.flog.level).to.be('quiet');
+      }
+    });
+  });
 });
